003455cfc2154aa0d5142e3b367599ec42b03786
Aiman Md Uslim <aiman.muslim@petronas.com>
2019-04-08 17:39:41 +0800

    started working on sign in page

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 // RegisterUserInfoPage includes
 // - Full name
 // - New IC Number
@@ -17,23 +16,21 @@ class RegisterUserInfoPage extends StatefulWidget {
 }
 
 bool _isNumeric(String str) {
-    if(str == null) {
-      return false;
-    }
-    return double.tryParse(str) != null;
+  if (str == null) {
+    return false;
   }
-
+  return double.tryParse(str) != null;
+}
 
 class RegisterUserInfoState extends State<RegisterUserInfoPage> {
   @override
-
   final _formKey = GlobalKey<FormState>();
 
   void initState() {
     super.initState();
   }
 
-  final passwordController =TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   void dispose() {
@@ -45,82 +42,85 @@ class RegisterUserInfoState extends State<RegisterUserInfoPage> {
   // TODO: need to validate only when enter is pressed not after every typing
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-      ),
-      body: SingleChildScrollView(child: 
-      Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            createFormField("Full Name", (value) {
-              
-            },
-          false,
-            null),
-            createFormField("New IC Number", (value) {
-              if (!value.isEmpty && !_isNumeric(value)) {
-                return 'Please enter digits only.';
-              }
-            },
-          false,
-            null),
-            createFormField("Email address", (value) {
-              if (!value.isEmpty && !RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
-                return 'Please enter a valid email address.';
-              }
-            },
-          false,
-            null),
-            createFormField("Password", (value) {
-              if (!value.isEmpty && value.length < 8) {
-                return 'Your password must be at least 8 characters';
-              }
-            },
-            true,
-            passwordController),
-            createFormField("Confirm password", (value) {
-              String enteredPassword =passwordController.text;
-              if(!value.isEmpty && enteredPassword != value) {
-                return "Passwords should match!";
-              }
-            },
-            true,
-            null),
-            createFormField("Phone Number", (value) {
-              if (!value.isEmpty && (!_isNumeric(value) || value.length != 10)) {
-                return "Please enter a valid phone number.";
-              }
-            }, 
-            false, 
-            null)
-
-          ],
+        appBar: AppBar(
+          title: Text("Register"),
         ),
-        
-      )
-      )
-      );
-  
+        body: SingleChildScrollView(
+            child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              createFormField("Full Name", (value) {}, false, null),
+              createFormField("New IC Number", (value) {
+                if (!value.isEmpty && !_isNumeric(value)) {
+                  return 'Please enter digits only.';
+                }
+              }, false, null),
+              createFormField("Email address", (value) {
+                if (!value.isEmpty &&
+                    !RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value)) {
+                  return 'Please enter a valid email address.';
+                }
+              }, false, null),
+              createFormField("Password", (value) {
+                if (!value.isEmpty && value.length < 8) {
+                  return 'Your password must be at least 8 characters';
+                }
+              }, true, passwordController),
+              createFormField("Confirm password", (value) {
+                String enteredPassword = passwordController.text;
+                if (!value.isEmpty && enteredPassword != value) {
+                  return "Passwords should match!";
+                }
+              }, true, null),
+              createFormField("Phone Number", (value) {
+                if (!value.isEmpty &&
+                    (!_isNumeric(value) || value.length != 10)) {
+                  return "Please enter a valid phone number.";
+                }
+              }, false, null)
+            ],
+          ),
+        )));
   }
 }
 
-Widget createFormField(String placeholder, Function validation, bool isPassword, TextEditingController controller) {
+class SignInPage extends StatefulWidget {
+  @override
+  SignInState createState() {
+    return SignInState();
+  }
+}
+
+class SignInState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Sign In"),
+            automaticallyImplyLeading: true,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context, false),
+            )
+            ),
+            
+            );
+  }
+}
+
+Widget createFormField(String placeholder, Function validation, bool isPassword,
+    TextEditingController controller) {
   return Padding(
-    padding: EdgeInsets.all(15),
-    child: TextFormField(
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
         validator: validation,
-        decoration: InputDecoration(
-          labelText: placeholder
-        ),
+        decoration: InputDecoration(labelText: placeholder),
         obscureText: isPassword,
         controller: controller,
         maxLines: null,
-      autovalidate: true,
-      )
-  );
-  
-  
-  
+        autovalidate: true,
+      ));
 }
