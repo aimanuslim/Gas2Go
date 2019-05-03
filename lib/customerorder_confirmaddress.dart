a3e4ab661337b91fd03dbd54fb77402c3892329c
Amirul Malek <amirulamalek@gmail.com>
2019-05-03 12:39:19 +0800

    Add Screen 1

--- lib/customerorder_confirmaddress.dart
+++ lib/customerorder_confirmaddress.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ConfirmOrder extends StatelessWidget{
+//Screen 13
+class ConfirmOrder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -8,42 +9,79 @@ class ConfirmOrder extends StatelessWidget{
         backgroundColor: Colors.black,
         leading: IconButton(
           tooltip: "Previous Screen",
-          icon: new Icon(Icons.arrow_back_ios, color: Colors.white,),
+          icon: new Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
           onPressed: null,
         ),
         title: new Text("Confirm Address"),
       ),
-
       body: new Container(
         child: Column(
           children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: <Widget>[
+                  new Icon(
+                    Icons.location_on,
+                    color: Colors.grey,
+                  ),
+                  new Text("Delivery Address"),
+                ],
+              ),
+            ),
             new Expanded(
               child: new Container(
-
+                child: Column(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: new Text(
+                              "No.18, Jalan SS15/2 Desa Petaling, Petaling Jaya 47305 Selangor"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-
             ),
-
+            new Expanded(
+              child: new SelectDifferentAddress(),
+            ),
             new Container(
               child: new Center(
                 child: FlatButton(
                   color: Colors.grey,
                   textColor: Colors.white,
-                  onPressed: ()=> debugPrint ("Confirm"),
-                  child: new Text("Confirm Address", 
-                  style: new TextStyle(
-                    fontSize: 12.3
-                  ) ,
+                  onPressed: () => debugPrint("Confirm"),
+                  child: new Text(
+                    "Confirm Address",
+                    style: new TextStyle(fontSize: 12.3),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
-
-      
     );
   }
+}
 
-} 
+class SelectDifferentAddress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: null,
+        child: new Container(
+          padding: const EdgeInsets.all(6),
+          decoration: BoxDecoration(color: Colors.white30),
+          child: new Text("Select a different address",
+              style: new TextStyle(color: Colors.blueAccent, fontSize: 12.1)),
+        ));
+  }
+}
