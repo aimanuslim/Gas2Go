0a6db1778f8191cd688f11a55ba208804c6e7b3c
FerdMas <afshafie93@gmail.com>
2019-06-25 17:35:49 +0800

    deleting unneeded import lines

--- lib/customer_screens.dart
+++ lib/customer_screens.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gas2go/texts.dart';
 import 'commonelements.dart';
-import 'customer_profile.dart';
-
-// void main() => runApp(MyApp());
 
 class HomeNav extends StatelessWidget {
   @override
@@ -521,6 +518,73 @@ Widget createFormField(String placeholder, Function validation) {
         autovalidate: true,
       ));
 }
+
+//screen 16a/b
+
+class DeliveryWindow extends StatefulWidget {
+  @override
+  DeliveryWindowState createState() => new DeliveryWindowState();
+}
+
+class DeliveryWindowState extends State<DeliveryWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          tooltip: "Previous Screen",
+          icon: new Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        title: new Text("Select delivery period"),
+      ),
+      body: new Container(
+          padding: new EdgeInsets.all(20.0),
+          child: ListView(
+            children: <Widget>[
+              RadioListTile(
+                title: Text("Deliver as soon as possible"),
+                subtitle: Text(
+                    "Same-day deliveries are only applicable for orders made by 12pm. Orders made after 12pm will be delivered the following day."),
+                value: "deliver-asap",
+                groupValue: "deliver-asap",
+                onChanged: (String value) {
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text("Deliver later"),
+                subtitle: Text(
+                    "Typically customers will receive their orders within 2 days"),
+                value: "deliver-later",
+                groupValue: "deliver-asap",
+                onChanged: (String value) {},
+              )
+            ],
+          )),
+    );
+  }
+}
+
+// for (int i = 0; i < 3; i++) {
+//   list.add(new RadioListTile(
+//     value: i,
+//     title: new Text('Radio $i'),
+//     groupValue: _selected,
+//     onChanged: (int value) {
+//       onChanged(value);
+//     },
+//     activeColor: Colors.red,
+//     secondary: new Icon(Icons.home),
+//     subtitle: new Text('Sub Title here'),
+//   ));
+// }
 
 // Home Tab - Screen 17 //
 
