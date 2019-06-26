bc813283164d54857dc0803b5a91d14402f259ad
Ahmad Ihsan <ihsanashi@gmail.com>
2019-06-26 23:55:18 +0800

    implemented a better way of displaying onboarding screens, as well as relevant dependencies and image assets

--- lib/customer_login.dart
+++ lib/customer_login.dart
@@ -1,271 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:transformer_page_view/transformer_page_view.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
 
 // void main() => runApp(MyApp());
 
 // Screens 2, 3, 4
-class MyHomePage extends StatefulWidget {
-  final String title;
-  MyHomePage({this.title});
+class Onboarding extends StatefulWidget {
   @override
-  MyHomePageState createState() {
-    return new MyHomePageState();
+  OnboardingState createState() {
+    return new OnboardingState();
   }
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  int _slideIndex = 0;
-  final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
-  final List<String> images = [
-    "assets/slide_1.png",
-    "assets/slide_2.png",
-    "assets/slide_3.png",
-  ];
+class OnboardingState extends State<Onboarding> {
+  List<Slide> slides = new List();
 
-  List<Color> colors = [Colors.orange];
-  final List<String> text0 = [
-    "Welcome in your app",
-    "Enjoy teaching...",
-    "Showcase your skills",
-  ];
-
-  final List<String> text1 = [
-    "App for food lovers, satisfy your taste",
-    "Find best meals in your area, simply",
-    "Have fun while eating with your relatives",
-  ];
-
-  final IndexController controller = IndexController();
   @override
-  Widget build(BuildContext context) {
-    TransformerPageView transformerPageView = TransformerPageView(
-        pageSnapping: true,
-        onPageChanged: (index) {
-          setState(() {
-            this._slideIndex = index;
-          });
-        },
-        loop: false,
-        controller: controller,
-        transformer: new PageTransformerBuilder(
-            builder: (Widget child, TransformInfo info) {
-          return new Material(
-            color: Colors.white,
-            elevation: 8.0,
-            textStyle: new TextStyle(color: Colors.white),
-            borderRadius: new BorderRadius.circular(12.0),
-            child: new Container(
-              alignment: Alignment.center,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new ParallaxContainer(
-                      child: new Text(
-                        text0[info.index],
-                        style: new TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 34.0,
-                            fontFamily: 'Quicksand',
-                            fontWeight: FontWeight.bold),
-                      ),
-                      position: info.position,
-                      opacityFactor: .8,
-                      translationFactor: 400.0,
-                    ),
-                    SizedBox(
-                      height: 45.0,
-                    ),
-                    new ParallaxContainer(
-                      child: new Image.asset(
-                        images[info.index],
-                        fit: BoxFit.contain,
-                        height: 350,
-                      ),
-                      position: info.position,
-                      translationFactor: 400.0,
-                    ),
-                    SizedBox(
-                      height: 45.0,
-                    ),
-                    new ParallaxContainer(
-                      child: new Text(
-                        text1[info.index],
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 28.0,
-                            fontFamily: 'Quicksand',
-                            fontWeight: FontWeight.bold),
-                      ),
-                      position: info.position,
-                      translationFactor: 300.0,
-                    ),
-                    SizedBox(
-                      height: 55.0,
-                    ),
-                    new ParallaxContainer(
-                      position: info.position,
-                      translationFactor: 500.0,
-                      child: Dots(
-                        controller: controller,
-                        slideIndex: _slideIndex,
-                        numberOfDots: images.length,
-                      ),
-                    ),
-                      new NextButtons(),
+  void initState() {
+    super.initState();
 
-                  ],
-                ),
-              ),
-            ),
-          );
-        }),
-        itemCount: 3);
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: transformerPageView,
+    slides.add(
+      Slide(
+        maxLineTitle: 200,
+        widthImage: 300,
+        heightImage: 300,
+        title: "EZ-ly order cooking gas",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        pathImage: "assets/onboarding_1_order.png",
+        backgroundColor: Color(0xff45569B),
+      ),
     );
-  }
-}
-
-class Dots extends StatelessWidget {
-  final IndexController controller;
-  final int slideIndex;
-  final int numberOfDots;
-  Dots({this.controller, this.slideIndex, this.numberOfDots});
-
-  List<Widget> _generateDots() {
-    List<Widget> dots = [];
-    for (int i = 0; i < numberOfDots; i++) {
-      dots.add(i == slideIndex ? _activeSlide(i) : _inactiveSlide(i));
-    }
-    return dots;
-  }
-
-  Widget _activeSlide(int index) {
-    return GestureDetector(
-      onTap: () {
-        print('Tapped');
-      },
-      child: new Container(
-        child: Padding(
-          padding: EdgeInsets.only(left: 8.0, right: 8.0),
-          child: Container(
-            width: 14.0,
-            height: 14.0,
-            decoration: BoxDecoration(
-              color: Colors.orangeAccent.withOpacity(.3),
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-          ),
-        ),
+    slides.add(
+      Slide(
+        maxLineTitle: 200,
+        widthImage: 300,
+        heightImage: 300,
+        title: "Our suppliers will deliver the goods",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        pathImage: "assets/onboarding_2_delivery.png",
+        backgroundColor: Color(0xff45569B),
+      ),
+    );
+    slides.add(
+      Slide(
+        maxLineTitle: 200,
+        widthImage: 300,
+        heightImage: 300,
+        title: "Invite friends and earn cashbacks",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. - (this feature is coming soon)",
+        pathImage: "assets/onboarding_3_cashbacks.png",
+        backgroundColor: Color(0xff45569B),
       ),
     );
   }
 
-  Widget _inactiveSlide(int index) {
-    return GestureDetector(
-      onTap: () {
-        controller.move(index);
-      },
-      child: new Container(
-        child: Padding(
-          padding: EdgeInsets.only(left: 5.0, right: 5.0),
-          child: Container(
-            width: 8.0,
-            height: 8.0,
-            decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(50.0)),
-          ),
-        ),
-      ),
-    );
+  void onDonePress() {
+    // TODO: go to next screen
+  }
+
+  void onSkipPress() {
+    // TODO: go to next screen
+    // CURRENTLY NOT WORKING
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: _generateDots(),
-    ));
+    return new IntroSlider(
+        slides: this.slides,
+        onDonePress: this.onDonePress,
+        onSkipPress: this.onSkipPress,
+        colorDot: Colors.black87,
+        colorActiveDot: Colors.white);
   }
 }
-
-class NextButtons extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 50.0),
-      child: Container(
-          child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(right: 10.0),
-              child: OutlineButton(
-                padding: EdgeInsets.all(16.0),
-                child: Text("Sign In"),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                onPressed: () => null,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                color: Colors.black,
-              ),
-              child: FlatButton(
-                // splashColor: Colors.blue,
-                child: Text(
-                  "Register",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () => null,
-              ),
-            ),
-          )
-        ],
-      )
-
-      // Old button implementation
-          /* child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              OutlineButton(
-                child: Text('Sign In'),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.black,
-                ),
-                child: FlatButton(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          ),
-          */
-          ),
-    );
-  }
-}
-
 
 // Screen 5a
 class CustomerSignInPage extends StatefulWidget {
@@ -334,10 +144,11 @@ class SignInScaffold extends StatelessWidget {
                   Padding(
                       padding: EdgeInsets.symmetric(horizontal: 15),
                       child: linkText("Forgot Password?", () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => ForgotPasswordPage()
-                        ));
-                      } ))
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ForgotPasswordPage()));
+                      }))
                 ],
               ),
               Column(
@@ -345,7 +156,9 @@ class SignInScaffold extends StatelessWidget {
                   Padding(
                       padding: EdgeInsets.symmetric(vertical: 5),
                       child: linkText(this.alternateSignInText, null)),
-                  new CommonProceedButton(text: "Sign In",),
+                  new CommonProceedButton(
+                    text: "Sign In",
+                  ),
                 ],
               )
               //   )
@@ -361,8 +174,6 @@ class CustomerRegisterPage extends StatefulWidget {
     return CustomerRegisterState();
   }
 }
-
-
 
 class CustomerRegisterState extends State<CustomerRegisterPage> {
   @override
@@ -433,11 +244,10 @@ class CustomerRegisterState extends State<CustomerRegisterPage> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         onPressed: () {
-          Navigator.push(context,
-            MaterialPageRoute(
-              builder: (context) => CustomerRegisterAddressPage()
-            )
-          );
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => CustomerRegisterAddressPage()));
         },
       ),
     );
@@ -489,5 +299,3 @@ class ForgotPasswordState extends State<ForgotPasswordPage> {
     );
   }
 }
-
-
