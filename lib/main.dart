d46b42de1188cd68e92a57b9da87a954e5aa0dd1
Ahmad Ihsan <ihsanashi@gmail.com>
2019-07-10 15:34:16 +0800

    partially added routes for navigating between files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'customer_screens.dart';
+import 'package:gas2go/customer_registration.dart';
+import 'package:gas2go/customer_sign_in.dart';
 
+import 'intro.dart';
+import 'onboarding.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gas 2 Go',
+      debugShowCheckedModeBanner: false,
+      title: 'EZ Gas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        backgroundColor: Color(0xff323E6E),
+        primaryColor: Color(0xff45569B),
+        accentColor: Color(0xffFFA630),
+        cardColor: Color(0xff3D4C8A),
+        bottomAppBarColor: Color(0xff3D4C8A),
+        buttonColor: Color(0xffFFA630),
+        textSelectionColor: Color(0xff222222),
       ),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: HomeNav(),
+      initialRoute: CustomerOnboarding.id,
+      routes: {
+        CustomerOnboarding.id: (context) => CustomerOnboarding(),
+        CustomerAppEntry.id: (context) => CustomerAppEntry(),
+        CustomerSignIn.id: (context) => CustomerSignIn(),
+        CustomerRegisterPartOne.id: (context) => CustomerRegisterPartOne(),
+        CustomerRegisterPartTwo.id: (context) => CustomerRegisterPartTwo(),
+        CustomerForgotPassword.id: (context) => CustomerForgotPassword(),
+      },
     );
   }
 }
-
