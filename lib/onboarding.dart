c836ccb14ad6d51eb4216ea6636216387289259c
Ahmad Ihsan <ihsanashi@gmail.com>
2019-07-21 14:26:02 +0800

    test edit

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -12,60 +12,6 @@ class CustomerOnboarding extends StatefulWidget {
 }
 
 class _CustomerOnboardingState extends State<CustomerOnboarding> {
-  // List<Slide> slides = new List();
-
-  // @override
-  // void initState() {
-  //   super.initState();
-
-  //   slides.add(
-  //     Slide(
-  //       maxLineTitle: 200,
-  //       widthImage: 300,
-  //       heightImage: 300,
-  //       title: "EZ-ly order cooking gas",
-  //       description:
-  //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //       pathImage: "assets/onboarding_1_order.png",
-  //     ),
-  //   );
-  //   Text("Test");
-  //   slides.add(
-  //     Slide(
-  //       maxLineTitle: 200,
-  //       widthImage: 300,
-  //       heightImage: 300,
-  //       title: "Our suppliers will deliver the goods",
-  //       description:
-  //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //       pathImage: "assets/onboarding_2_delivery.png",
-  //     ),
-  //   );
-  //   slides.add(
-  //     Slide(
-  //       maxLineTitle: 200,
-  //       widthImage: 300,
-  //       heightImage: 300,
-  //       title: "Invite friends and earn cashbacks",
-  //       description:
-  //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. - (this feature is coming soon)",
-  //       pathImage: "assets/onboarding_3_cashbacks.png",
-  //     ),
-  //   );
-  // }
-
-  // void onDonePress() {
-  //   Navigator.pushNamed(context, '/customer/appEntry');
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return new IntroSlider(
-  //     slides: this.slides,
-  //     onDonePress: this.onDonePress,
-  //   );
-  // }
-
   List<Slide> slides = new List();
 
   @override
